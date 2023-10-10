c4f6418c8ca7a7a0a09e16bee5a06584251f0f7a
gnomemage1990@gmail.com <gnomemage1990@gmail.com>
2023-10-10 21:41:46 +0800

    contact page

--- pages/contact.js
+++ pages/contact.js
@@ -8,35 +8,35 @@ export default function Contact() {
     const [data, setData] = useState([
         {
             id: 1,
-            image_path: "/images/contact1.jpg",
-            name: 'IDA',
+            image_path: "/images/contact1.png",
+            name: 'SYAFIQ',
             role: "Sales Support",
-            tel: "+601135108412",
-            email: "ida@kakijalan.com.my"
+            tel: "+6011-1093 4828",
+            email: "demo@gmail.com"
         },
         {
             id: 2,
-            image_path: "/images/contact2.jpg",
-            name: 'ZIZA',
-            role: "Sales Support (Corporate)",
-            tel: "+601135178423",
-            email: "ziza@kakijalan.com.my"
+            image_path: "/images/contact2.png",
+            name: 'HAFIS',
+            role: "Sales Support",
+            tel: "+6011-1091 4828",
+            email: "demo@gmail.com"
         },
         {
             id: 3,
-            image_path: "/images/contact3.jpg",
-            name: 'MASYITAH',
+            image_path: "/images/contact3.png",
+            name: 'EIDA',
             role: "Sales Support",
-            tel: "+601155548280",
-            email: "masyitah@kakijalan.com.my"
+            tel: "+6011-3517 8412",
+            email: "demo@gmail.com"
         },
         {
             id: 4,
-            image_path: "/images/contact4.jpg",
-            name: 'ALYA',
-            role: "Sales Support (Agency Sales)",
-            tel: "+60178812782",
-            email: "alya@kakijalan.com.my "
+            image_path: "/images/contact4.png",
+            name: 'NADIA',
+            role: "Sales Support",
+            tel: "+6011-1094 4828",
+            email: "demo@gmail.com"
         },
     ])
 
@@ -48,9 +48,8 @@ export default function Contact() {
                 id="banner"
                 className="bg-image d-flex justify-content-center align-items-start"
                 style={{
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                borderBottom: "15px solid #b32129"
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
                 }}
             >
                 <div
@@ -100,136 +99,147 @@ export default function Contact() {
                     </h5>
                     <div className="col-12 col-xl-6">
                         <p className="fw-bold m-1" style={{ fontFamily: 'Poppins' }}>
-                        First Name <span style={{ color: "#ea242d" }}>*</span>
+                            First Name <span style={{ color: "#ea242d" }}>*</span>
                         </p>
                         <input
-                        type="text"
-                        className="form-control"
-                        placeholder=""
-                        id="fname"
+                            type="text"
+                            className="form-control"
+                            placeholder="John"
+                            id="fname"
                         />
                     </div>
                     <div className="col-12 col-xl-6">
                         <p className="fw-bold m-1" style={{ fontFamily: 'Poppins' }}>
-                        Surname <span style={{ color: "#ea242d" }}>*</span>
+                            Surname <span style={{ color: "#ea242d" }}>*</span>
                         </p>
                         <input
-                        type="text"
-                        className="form-control"
-                        placeholder=""
-                        id="sname"
+                            type="text"
+                            className="form-control"
+                            placeholder="Doe"
+                            id="sname"
                         />
                     </div>
                     <div className="col-12 col-xl-6">
                         <p className="fw-bold m-1" style={{ fontFamily: 'Poppins' }}>
-                        Contact Name <span style={{ color: "#ea242d" }}>*</span>
+                            Contact Name <span style={{ color: "#ea242d" }}>*</span>
                         </p>
                         <input
-                        type="tel"
-                        className="form-control"
-                        placeholder=""
-                        id="contact"
+                            type="tel"
+                            className="form-control"
+                            placeholder="0123456789"
+                            id="contact"
                         />
                     </div>
                     <div className="col-12 col-xl-6">
                         <p className="fw-bold m-1" style={{ fontFamily: 'Poppins' }}>
-                        Email <span style={{ color: "#ea242d" }}>*</span>
+                            Email <span style={{ color: "#ea242d" }}>*</span>
                         </p>
                         <input
-                        type="email"
-                        className="form-control"
-                        placeholder=""
-                        id="email"
+                            type="email"
+                            className="form-control"
+                            placeholder="Email"
+                            id="email"
                         />
                     </div>
                     <div className="col-12">
                         <p className="fw-bold m-1" style={{ fontFamily: 'Poppins' }}>
-                        Message
+                            Message
                         </p>
                         <textarea
-                        className="form-control"
-                        id="exampleFormControlTextarea1"
-                        rows={3}
-                        defaultValue={""}
+                            className="form-control"
+                            id="exampleFormControlTextarea1"
+                            placeholder="Write Message"
+                            rows={3}
+                            defaultValue={""}
                         />
                     </div>
                     <button
                         type="submit"
-                        className="col-12 col-xl-3 btn text-white fw-bold rounded-5 mt-4 mt-xl-5"
-                        style={{ backgroundColor: "#ea242d", fontFamily: '"Montserrat"' }}
+                        className="col-12 col-xl-3 btn text-white fw-bold rounded-3 mt-4 mt-xl-5"
+                        style={{ backgroundColor: "#3470C9", fontFamily: '"Montserrat"' }}
                     >
-                        SUBMIT
+                        SEND
                     </button>
                     </div>
                 </form>
                 <div
                     id="message_desc_div"
                     className="d-flex flex-column gap-4 p-5 col-12 col-xl-4 justify-content-xl-center"
-                    style={{ backgroundColor: "#b8b09d" }}
+                    style={{ backgroundColor: "#3470C9" }}
                 >
-                    <div className="d-flex flex-row gap-4">
-                    <img 
-                        src="/icons/phone.png"
-                        alt="phone"
-                        className="mt-2 contact-icon"
-                    />
-                    <div className="d-flex flex-column flex-wrap">
-                        <p
+                    <p
                         className="fw-bold mb-2"
                         style={{
-                            color: "#e8e2d6",
+                            color: "#fff",
                             fontFamily: '"Montserrat"',
-                            letterSpacing: "0.15em"
+                            letterSpacing: "0.20em"
                         }}
-                        >
-                        CALL US
-                        </p>
-                        <Link 
-                        href="tel:+60360960384"
-                        className="text-decoration-none text-white"
-                        >
-                        <p
-                            className="text-white fw-bold m-0"
-                            style={{ fontFamily: 'Poppins', letterSpacing: 1 }}
-                        >
-                            +60 3 6096 0384
-                        </p>
-                        </Link>
-                    </div>
-                    </div>
+                    >
+                        CONTACT INFORMATION
+                    </p>
                     <div className="d-flex flex-row gap-4">
-                    <img 
-                        src="/icons/email.png"
-                        alt="email"
-                        className="mt-2 contact-icon"
-                    />
-                    <div className="d-flex flex-column flex-wrap">
-                        <p
-                        className="fw-bold mb-2"
-                        style={{
-                            color: "#e8e2d6",
-                            fontFamily: '"Montserrat"',
-                            letterSpacing: "0.15em"
-                        }}
-                        >
-                        EMAIL US
-                        </p>
-                        <Link 
-                        href="mailto:support@kakijalan.com.my"
-                        className="text-decoration-none text-white"
-                        >
-                        <p
-                            className="text-white fw-bold m-0"
-                            style={{
-                            fontFamily: 'Poppins',
-                            letterSpacing: 1,
-                            overflowWrap: "anywhere"
-                            }}
-                        >
-                            support@kakijalan.com.my
-                        </p>
-                        </Link>
-                    </div>
+                        <img 
+                            src="/icons/phone.png"
+                            alt="phone"
+                            className="mt-2 contact-icon"
+                        />
+                        <div className="d-flex flex-column flex-wrap">
+                            <p
+                                className="fw-normal mb-2"
+                                style={{
+                                    color: "#fff",
+                                    fontFamily: '"Montserrat"',
+                                    fontSize: "0.8rem"
+                                }}
+                            >
+                                CALL US
+                            </p>
+                            <Link 
+                                href="tel:+60360960384"
+                                className="text-decoration-none text-white"
+                            >
+                            <p
+                                className="text-white fw-normal m-0"
+                                style={{ fontFamily: 'Poppins', letterSpacing: 1 }}
+                            >
+                                +60 3 6096 0384
+                            </p>
+                            </Link>
+                        </div>
+                        </div>
+                        <div className="d-flex flex-row gap-4">
+                        <img 
+                            src="/icons/email.png"
+                            alt="email"
+                            className="mt-2 contact-icon"
+                        />
+                        <div className="d-flex flex-column flex-wrap">
+                            <p
+                                className="fw-normal mb-2"
+                                style={{
+                                    color: "#fff",
+                                    fontFamily: '"Montserrat"',
+                                    fontSize: "0.8rem"
+                                }}
+                            >
+                                EMAIL US
+                            </p>
+                            <Link 
+                                href="mailto:support@kakijalan.com.my"
+                                className="text-decoration-none text-white"
+                            >
+                            <p
+                                className="text-white fw-normal m-0"
+                                style={{
+                                    fontFamily: 'Poppins',
+                                    letterSpacing: 1,
+                                    overflowWrap: "anywhere"
+                                }}
+                            >
+                                support@kakijalan.com.my
+                            </p>
+                            </Link>
+                        </div>
                     </div>
                     <div className="d-flex flex-row gap-4">
                     <img 
@@ -239,38 +249,23 @@ export default function Contact() {
                     />
                     <div className="d-flex flex-column flex-wrap">
                         <p
-                        className="fw-bold mb-2"
-                        style={{
-                            color: "#e8e2d6",
-                            fontFamily: '"Montserrat"',
-                            letterSpacing: "0.15em"
-                        }}
+                            className="fw-normal mb-2"
+                            style={{
+                                color: "#fff",
+                                fontFamily: '"Montserrat"',
+                                fontSize: "0.8rem"
+                            }}
                         >
-                        OUR LOCATION
+                            OUR LOCATION
                         </p>
                         <p
-                        className="text-white fw-bold m-0"
-                        style={{ fontFamily: 'Poppins', letterSpacing: "0.5px" }}
+                            className="text-white fw-normal m-0"
+                            style={{ fontFamily: 'Poppins', letterSpacing: "0.5px" }}
                         >
-                        29-1, JALAN ESERINA AA U16/AA ELMINA, SEKSYEN U16 40160 SHAH ALAM
-                        SELANGOR
+                            29-1, JALAN ESERINA AA U16/AA ELMINA, SEKSYEN U16 40160 SHAH ALAM
+                            SELANGOR
                         </p>
                     </div>
-                    </div>
-                    <div className="d-flex flex-row gap-4">
-                    <Link 
-                        href="https://www.facebook.com"
-                        style={{ width: 30 }}
-                        className="d-flex justify-content-center"
-                    >
-                        <img  src="/icons/facebook_white.png" alt="facebook" />
-                    </Link>
-                    <Link href="https://www.instagram.com">
-                        <img  src="/icons/instagram_white.png" alt="instagram" />
-                    </Link>
-                    <Link href="https://www.youtube.com">
-                        <img  src="/icons/youtube_white.png" alt="youtube" />
-                    </Link>
                     </div>
                 </div>
                 </div>
@@ -281,18 +276,30 @@ export default function Contact() {
                     {
                         data.map((value,index)=>{
                             return (
-                                <div className="d-flex flex-column justify-content-center align-items-center gap-2 mt-5" key={index}>
+                                <div className="d-flex flex-column justify-content-center align-items-center gap-2 p-8 mt-5 shadow-lg shadow-cyan-500/50" key={index}>
                                     <img  src={value.image_path} width="200px" style={{borderTopLeftRadius: '1.5rem', backgroundSize: 'cover'}} alt="Article Image" />
+                                    <Link 
+                                        className="d-flex flex-row justify-content-center align-items-center gap-2 text-decoration-none relative top-[-50px] left-[70px]"
+                                        href={`https://wa.me/${value.tel.replace(/\D/g, "")}?text=Hi,%20this%20is%20from%20KakiJalan" style="cursor: pointer;`}
+                                    >
+                                        <img src="/icons/whatsapp.png" width="25px" alt="whatsapp" />
+                                    </Link>
                                     <p className="m-0 fw-bold" style={{fontFamily: '"Montserrat"'}}>{value.name}</p>
                                     <div className="text-center" style={{fontFamily: 'Poppins'}}>
                                         <p className="m-0 fst-italic" style={{color: '#A0A0A0'}}>{value.role}</p>
-                                        <p className="m-0 fw-bold" style={{color: '#B32129'}}>{value.tel}</p>
-                                        <Link href="mailto:${e.email}" className="m-0 text-decoration-none" style={{color: '#B32129'}}>{value.email}</Link>
+                                        <p className="mt-4 fw-bold" style={{color: '#000', fontSize:"0.85rem" }}>
+                                            <div className="flex flex-row">
+                                                <img src="icons/contact_phonecall.png" className="mr-2" />
+                                                {value.tel}
+                                            </div>
+                                        </p>
+                                        <Link href="mailto:${e.email}" className="m-0 text-decoration-none" style={{color: '#000', fontSize:"0.85rem"}}>
+                                            <div className="flex flex-row">
+                                                <img src="icons/contact_email.png"  className="mr-2" />
+                                                {value.email}
+                                            </div>
+                                        </Link>
                                     </div>
-                                    <Link className="d-flex flex-row justify-content-center align-items-center gap-2 text-decoration-none" href={`https://wa.me/${value.tel.replace(/\D/g, "")}?text=Hi,%20this%20is%20from%20KakiJalan" style="cursor: pointer;`}>
-                                        <img src="/icons/whatsapp.png" width="50px" alt="whatsapp" />
-                                        <p className="m-0 fw-bold" style={{fontFamily: 'Poppins', color: '#A0A0A0'}}>Whatsapp</p>
-                                    </Link>
                                 </div>
                             )
                         })
